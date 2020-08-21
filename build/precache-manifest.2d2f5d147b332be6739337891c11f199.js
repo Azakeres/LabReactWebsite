55cd9871073daed996173b1623f3f95ae9844982
Arash Zakeresfahani <azakeres@iu.edu>
2020-08-21 17:05:07 -0400

    testing travis4

---
+++ build/precache-manifest.2d2f5d147b332be6739337891c11f199.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8afd27e33acd2b382944ef01282681",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d599a7639a993254aa48",
+    "url": "/static/css/2.a09d231f.chunk.css"
+  },
+  {
+    "revision": "b3b2b31615f716551535",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d599a7639a993254aa48",
+    "url": "/static/js/2.def5564b.chunk.js"
+  },
+  {
+    "revision": "4d833975c154ea21126df2a4e5867037",
+    "url": "/static/js/2.def5564b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b2b31615f716551535",
+    "url": "/static/js/main.72187187.chunk.js"
+  },
+  {
+    "revision": "b1709356ac4b9c76784d",
+    "url": "/static/js/runtime-main.b8ef355b.js"
+  },
+  {
+    "revision": "0e84d5926187e7a3b785febd3a55bf1d",
+    "url": "/static/media/Ins.0e84d592.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4396b22d8d2866209e501fb93d8d25c8",
+    "url": "/static/media/members.4396b22d.csv"
+  }
+]);
